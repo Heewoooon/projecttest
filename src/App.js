fe225736776bbfea0d18cf26d57922a02c1a532b
heewon <kimhw3934@naver.com>
2024-05-07 12:31:19 +0900

    test

--- src/App.js
+++ src/App.js
@@ -8,19 +8,9 @@ function App() {
       <p>너무 설렌다 이거예요~ 우리 팀원이 이걸 보고 참 고생 많이 하는 팀장이구나 생각해주면 좋겠어요~</p>
       <h3>안뇽안뇽! 내가 바로 공유를 해볼겡! ㅎㅎㅎ 다들 꿀 빨고 있어?</h3>
       <h1>달다 달어</h1>
-<<<<<<< HEAD
       <h3>태은이가 좋아하는 랜덤 게임? 무슨 게임? 게임 스타트! 게임 스타트!</h3>
       <img src='https://i.ytimg.com/vi/Vmsnj_eluoY/maxresdefault.jpg'>그려그려</img>
     </div>
   );
-=======
-      <img src='https://i.ytimg.com/vi/Vmsnj_eluoY/maxresdefault.jpg'></img>
-      <h1>그려그려</h1>
 
-      <h1>으휴 비밀번호도 몰라요</h1>
-      </div>
-  )
->>>>>>> 3488a49db3b517788ddea45aac70e8ed4d7d6cd8
-}
 
-export default App;
