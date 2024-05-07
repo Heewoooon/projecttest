a21c83c39498d3670e627bec6cd1ddbb158beb15
KSH <bom5928@gmail.com>
2024-05-07 12:08:44 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <p>너무 설렌다 이거예요~ 우리 팀원이 이걸 보고 참 고생 많이 하는 팀장이구나 생각해주면 좋겠어요~</p>
       <h3>안뇽안뇽! 내가 바로 공유를 해볼겡! ㅎㅎㅎ 다들 꿀 빨고 있어?</h3>
       <h1>달다 달어</h1>
-      <img src='https://i.ytimg.com/vi/Vmsnj_eluoY/maxresdefault.jpg'>그려그려</img>
+      <img src='https://i.ytimg.com/vi/Vmsnj_eluoY/maxresdefault.jpg'></img>
 
       <h1>으휴 비밀번호도 몰라요</h1>
     </div>
