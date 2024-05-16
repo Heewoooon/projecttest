8baaafd553367055fde98b8c75d603996cd332f2
tlqkf <bom5928@gmail.com>
2024-05-16 17:56:17 +0900

    heewonllast

--- react-project/src/components/Main.jsx
+++ react-project/src/components/Main.jsx
@@ -5,23 +5,30 @@ import './MainStyles.css'
 const Main = () => {
   // console.log(process.env.PUBLIC_URL)
   return (
-    <div className="card-group ">
-      <div className="card cardbox1" >
+    <div>
+
+   <div class="card-group">   
+     <div className="card cardbox1" >
         <img src='/dogcomu.png' className="card-img-top"  />
         <div className="card-body">
           <a href="#" className="btn btn-primary">반려동물 게시판</a>
           <p class="card-text">설명1</p>
         </div>
       </div>
-
-      <div className="card cardbox2" >
+      </div>
+  
+<div>      
+  <div className="card cardbox2" >
         <img src="/dogweight.png" className="card-img-top" />
         <div className="card-body">
           <a href="#" className="btn btn-primary">반려동물 체중계</a>
           <p class="card-text">설명1</p>
         </div>
       </div>
-    </div>
+      </div>
+
+      </div>
+   
   );
 };
 
