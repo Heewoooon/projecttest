833ae35b53b49241d64a06767366fb50f3e4bd81
tlqkf <bom5928@gmail.com>
2024-05-16 17:49:36 +0900

    dogdog

--- react-project/src/components/Main.jsx
+++ react-project/src/components/Main.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './MainStyles.css'
+
 
 const Main = () => {
+  // console.log(process.env.PUBLIC_URL)
   return (
-    <div>
-      Main 페이지입니다.
-    </div>
-  )
-}
+    <div className="card-group ">
+      <div className="card cardbox1" >
+        <img src='/dogcomu.png' className="card-img-top"  />
+        <div className="card-body">
+          <a href="#" className="btn btn-primary">반려동물 게시판</a>
+          <p class="card-text">설명1</p>
+        </div>
+      </div>
 
-export default Main
+      <div className="card cardbox2" >
+        <img src="/dogweight.png" className="card-img-top" />
+        <div className="card-body">
+          <a href="#" className="btn btn-primary">반려동물 체중계</a>
+          <p class="card-text">설명1</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
